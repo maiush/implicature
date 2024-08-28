d4095809f78b877abe06c7b189b856ed76faf740
Sharan Maiya <29641373+maiush@users.noreply.github.com>
2024-08-28 23:09:30 +0100

    add final results, remove activation steering attempt

--- constants.py
+++ constants.py
@@ -3,6 +3,9 @@ results_path = "/gws/nopw/j04/ai4er/users/maiush/implicature_results"
 llm_cache = "/gws/nopw/j04/ai4er/users/maiush/LLMs"
 
 models = {
+    "qwen-2-72b-instruct": "Qwen/Qwen2-72B-Instruct", # 72b
+    "qwen-2-72b-base": "Qwen/Qwen2-72B", # 72b
+    
     "mistral-nemo-12b-instruct": "mistralai/Mistral-Nemo-Instruct-2407", # 12b
     "mistral-nemo-12b-base": "mistralai/Mistral-Nemo-Base-2407", # 12b
 
